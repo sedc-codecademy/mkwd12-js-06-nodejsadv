a074e8951ed80a3f856d5267ecad1eeef66f8089
Ivo Kostovski <ivo.kostovski@gmail.com>
2024-03-27 22:15:16 +0100

    add comments and how to

---
+++ G1/HOW_TO.md
@@ -0,0 +1,31 @@
+# How to...
+
+## Create a Database in Mongo DB?
+1. Go to mongodb.com
+2. Log in (if you don't have an account already, create one using the `Sign Up` button and choosing Google.)
+3. Click on `Browse Collections` within the Cluster0 tile*
+4. Collections tab will be opened, and you should see `+ Create Database` within the left sidebar
+5. Add database name
+6. Add collection name
+7. Click `Create`
+8. Celebrate!
+
+## Connect to a Mongo DB Database?
+1. Go to mongodb.com
+2. Log in
+3. Click on `Connect` within the Cluster0 tile*
+4. Click on `Connect your application`
+5. Copy the connection string to your app
+6. Change the password
+7. Celebrate!
+
+* If you are not seeing this tile, you will need the create a Cluster first
+
+## Add a new user to a Cluster within Mongo DB
+1. Go to mongodb.com
+2. Log in
+3. Click on `Add new database user`
+4. Choose `password`
+5. Add username and password - DO NOT USE the following characters `: / ? # [ ] @`
+6. Click add user
+7. Celebrate!
--- G1/class02-mongodb/src/services/movie.service.js
+++ G1/class02-mongodb/src/services/movie.service.js
@@ -1,28 +1,29 @@
-import { v4 as uuid } from 'uuid';
-import { getDb } from '../database/mongo-connection.js';
-import { ObjectId } from 'mongodb';
+import {getDb} from '../database/mongo-connection.js';
+import {ObjectId} from 'mongodb';
 
 export class MovieService {
-	static movies = [];
 
-	static async getAllMovies({ releaseYear, rating }) {
+	static getAllMovies({ releaseYear, rating }) {
+		// we can use the searchQuery object to build the query dynamically
 		let searchQuery = {};
 
+		// if we have a releaseYear, we add it to the searchQuery object and filter by it
 		if (releaseYear) {
 			searchQuery.releaseYear = Number(releaseYear);
 		}
 
+		// if we have a rating, we add it to the searchQuery object and filter by it
 		if (rating) {
 			searchQuery.rating = Number(rating);
 		}
 
-		const movies = await getDb()
-			.collection('movies')
-			.find(searchQuery)
-			.toArray();
+		// we return the result of the query, which will be an array of movies filtered by the searchQuery object
+		return getDb()
+				.collection('movies')
+				.find(searchQuery)
+				.toArray();
 
-		return movies;
-
+		// We avoid this code (below) because it's not efficient. Database should do the filtering, not the application (server).
 		// if (!releaseYear) {
 		// 	return movies;
 		// }
@@ -31,16 +32,23 @@ export class MovieService {
 	}
 
 	static getMovieById(movieId) {
+		// we return the result of the query, which will be the movie with the specified id
+		// movieId is a string, so we need to convert it to an ObjectId
+		// we use the ObjectId class from the mongodb package to create an ObjectId from the movieId string
+		// findOne will return the first movie that matches the query, or null if there are no matches
 		return getDb()
 			.collection('movies')
 			.findOne({ _id: new ObjectId(movieId) });
 	}
 
 	static createMovie(movieData) {
+		// insertOne will insert the movieData object into the movies collection
 		return getDb().collection('movies').insertOne(movieData);
 	}
 
 	static updateMovie(movieId, updateData) {
+		// updateOne will update the movie with the specified id with the updateData object
+		// $set is a mongodb operator that sets the fields in the updateData object
 		return getDb()
 			.collection('movies')
 			.updateOne(
@@ -52,6 +60,7 @@ export class MovieService {
 	}
 
 	static deleteMovie(movieId) {
+		// deleteOne will delete the movie with the specified id
 		return getDb()
 			.collection('movies')
 			.deleteOne({
