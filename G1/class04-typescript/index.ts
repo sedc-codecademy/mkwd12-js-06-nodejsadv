cf11c9115076290759c24a5038a3e6a96a3ffc38
Ivo Kostovski <ivo.kostovski@gmail.com>
2024-04-01 18:35:38 +0200

    init ts

---
+++ G1/class04-typescript/index.ts
@@ -0,0 +1,9 @@
+// Primitive Types
+const isDone: boolean = true;
+const age: number = 30;
+const percentage: number = 1.5;
+const color: string = 'green';
+let carOwner: undefined = undefined;
+let catName: null = null;
+
+// catName = 'something';
