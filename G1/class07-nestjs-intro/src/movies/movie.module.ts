d7f7dfa6bd51e5dee1259335d03a622a5e9db18e
Ivo Kostovski <ivo.kostovski@gmail.com>
2024-04-08 22:20:22 +0200

    add movies example

--- G1/class07-nestjs-intro/src/movies/movie.module.ts
+++ G1/class07-nestjs-intro/src/movies/movie.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MovieController } from './movie.controller';
+import { MovieService } from './movie.service';
 
 // config object for the module
 @Module({
   imports: [], // other modules
   controllers: [MovieController], // controllers
-  providers: [], // services, custom decorators
+  providers: [MovieService], // services, custom decorators
 })
 export class MovieModule {}
