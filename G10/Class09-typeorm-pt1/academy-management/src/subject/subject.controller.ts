cde228d68c01250f2824fda0b4e762236354021b
anetastankovska <aneta.stankovska@cw.bluedotcorp.com>
2024-04-15 20:34:27 +0200

    finish class 09

--- G10/Class09-typeorm-pt1/academy-management/src/subject/subject.controller.ts
+++ G10/Class09-typeorm-pt1/academy-management/src/subject/subject.controller.ts
@@ -1,34 +1,46 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { SubjectService } from './subject.service';
 import { CreateSubjectDto } from './dto/create-subject.dto';
 import { UpdateSubjectDto } from './dto/update-subject.dto';
+import { Subject } from './entities/subject.entity';
 
 @Controller('subject')
 export class SubjectController {
   constructor(private readonly subjectService: SubjectService) {}
 
-  @Post()
-  create(@Body() createSubjectDto: CreateSubjectDto) {
-    return this.subjectService.create(createSubjectDto);
-  }
-
   @Get()
-  findAll() {
+  findAll(): Promise<Subject[]> {
     return this.subjectService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id') id: string): Promise<Subject> {
     return this.subjectService.findOne(+id);
   }
 
+  @Post()
+  create(@Body() createSubjectDto: CreateSubjectDto): Promise<Subject> {
+    return this.subjectService.create(createSubjectDto);
+  }
+
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSubjectDto: UpdateSubjectDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateSubjectDto: UpdateSubjectDto,
+  ): Promise<Subject> {
     return this.subjectService.update(+id, updateSubjectDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id') id: string): Promise<void> {
     return this.subjectService.remove(+id);
   }
 }
--- G10/Class09-typeorm-pt1/academy-management/src/subject/subject.service.ts
+++ G10/Class09-typeorm-pt1/academy-management/src/subject/subject.service.ts
@@ -1,26 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateSubjectDto } from './dto/create-subject.dto';
 import { UpdateSubjectDto } from './dto/update-subject.dto';
+import { Subject } from './entities/subject.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class SubjectService {
-  create(createSubjectDto: CreateSubjectDto) {
-    return 'This action adds a new subject';
+  constructor(
+    @InjectRepository(Subject)
+    private subjectRepository: Repository<Subject>,
+  ) {}
+
+  async findAll(): Promise<Subject[]> {
+    return this.subjectRepository.find();
   }
 
-  findAll() {
-    return `This action returns all subject`;
+  async findOne(id: number): Promise<Subject> {
+    // findOneByOrFail from a repository typically throws an EntityNotFoundError if no entity is found with the provided criteria
+    try {
+      return this.subjectRepository.findOneByOrFail({ id });
+    } catch (error) {
+      if (error.name === 'EntityNotFoundError') {
+        throw new NotFoundException(`Subject with ID ${id} not found`);
+      }
+      throw error; // rethrow the error if it's not the expected error type
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} subject`;
+  async create(createSubjectDto: CreateSubjectDto): Promise<Subject> {
+    const subject = this.subjectRepository.create(createSubjectDto);
+    await this.subjectRepository.save(subject);
+    return subject;
   }
 
-  update(id: number, updateSubjectDto: UpdateSubjectDto) {
-    return `This action updates a #${id} subject`;
+  async update(
+    id: number,
+    updateSubjectDto: UpdateSubjectDto,
+  ): Promise<Subject> {
+    let subject = await this.subjectRepository.findOneBy({ id });
+    subject = this.subjectRepository.merge(subject, updateSubjectDto);
+    await this.subjectRepository.save(subject);
+    return subject;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} subject`;
+  async remove(id: number): Promise<void> {
+    await this.subjectRepository.delete(id);
   }
 }
