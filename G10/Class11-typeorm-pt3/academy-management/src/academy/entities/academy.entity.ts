644a40c942787933543318e0def8f4104bf07dfb
anetastankovska <aneta.stankovska@cw.bluedotcorp.com>
2024-04-19 21:05:49 +0200

    add class code

---
+++ G10/Class11-typeorm-pt3/academy-management/src/academy/entities/academy.entity.ts
@@ -0,0 +1,30 @@
+import { Student } from 'src/student/entities/student.entity';
+import { Subject } from 'src/subject/entities/subject.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Academy {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  startDate: Date;
+
+  @Column()
+  endDate: Date;
+
+  @Column()
+  price: number;
+
+  @OneToMany(() => Subject, (subject) => subject.academy, { cascade: true })
+  subjects: Subject[];
+
+  @OneToMany(() => Student, (student) => student.academy, { cascade: true })
+  students: Student[];
+}
