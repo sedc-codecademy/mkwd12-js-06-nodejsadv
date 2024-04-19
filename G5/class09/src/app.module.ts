2e068102f61d3ab02b5283ca5ceccd7fc332b10c
Iva-Cvetkovska <ivacvetkovska.freelance@gmail.com>
2024-04-19 18:42:24 +0200

    Add club entity

--- G5/class09/src/app.module.ts
+++ G5/class09/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { PlayersModule } from './players/players.module';
+import { ClubsModule } from './clubs/clubs.module';
 
 @Module({
-  imports: [DatabaseModule, PlayersModule],
+  imports: [DatabaseModule, PlayersModule, ClubsModule],
   controllers: [],
   providers: [],
 })
--- G5/class09/src/clubs/clubs.controller.ts
+++ G5/class09/src/clubs/clubs.controller.ts
@@ -15,19 +15,28 @@ import { ClubUpdateDto } from './dtos/club-update.dto';
 
 @Controller('clubs')
 export class ClubsController {
-    // constructor(private readonly clubsService: ClubsService) { }
+    // public clubsService: ClubsService
+    constructor(private readonly clubsService: ClubsService) { 
+        // clubsService = new ClubsService()
+    }
 
-    // @Get()
-    // getClubs(): Promise<Club[]> {}
+    @Get()
+    getClubs(): Promise<Club[]> {
+        return this.clubsService.getClubs();
+    }
 
-    // @Post('/')
-    // createClub(@Body() body: ClubCreateDto): Promise<Club> {}
+    @Post('/')
+    createClub(@Body() body: ClubCreateDto): Promise<Club> {
+        return this.clubsService.createClub(body);
+    }
 
-    // @Put('/:id')
-    // updateClub(
-    //     @Param('id') id: string,
-    //     @Body() body: ClubUpdateDto,
-    // ): Promise<Club> { }
+    @Put('/:id')
+    updateClub(
+        @Param('id') id: string,
+        @Body() body: ClubUpdateDto,
+    ): Promise<Club> { 
+        return this.clubsService.updateClub(id, body)
+    }
 
     // @Delete('/:id')
     // deleteClub(@Param('id') id: string): Promise<void> {}
