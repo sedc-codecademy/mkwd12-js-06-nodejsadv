cbb102cd6b3fad35cd5329e98068594c840adf66
Ivo Kostovski <ivo.kostovski@gmail.com>
2024-05-10 17:53:47 +0200

    init app

--- G1/class15-workshop/company_management/src/main.ts
+++ G1/class15-workshop/company_management/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Company Management')
+    .setDescription('Company Management Swagger')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  app.setGlobalPrefix('/api');
+
   await app.listen(3000);
 }
 bootstrap();
